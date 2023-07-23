d9c55430d2b645116dc1c6b601f95be1333ca25c
kleiry20 <Anushkasingh801@gmail.com>
2023-07-23 15:42:27 +0530

    mobile sidenav fixes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import { Drawer, Button, Menu } from "antd";
+import { Drawer, Button, Menu, Tag, Divider } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import "./NavBar.css";
 import HamburgerIcon from "../../assets/hamburger.svg";
+
+import DashboardIcon from "../../assets/dashboardIcon.svg";
+import SiderAssessmentIcon from "../../assets/siderAssessment.svg";
+import LibraryIcon from "../../assets/libraryIcon.svg";
 
 const NavBar = ({ menu }: any) => {
   const [visible, setVisible] = useState(false);
@@ -25,7 +29,7 @@ const NavBar = ({ menu }: any) => {
         }}
       />
       <Drawer
-        title="Topics"
+        title="Menu"
         placement="left"
         onClick={() => setVisible(false)}
         onClose={() => setVisible(false)}
@@ -33,10 +37,90 @@ const NavBar = ({ menu }: any) => {
         className="mobile-view-menu"
       >
         {/* {menu} */}
-        <Menu mode="inline" className="mobile-view-menu">
-          <Menu.Item>DashBoard </Menu.Item>
-          <Menu.Item>Assessment </Menu.Item>
-          <Menu.Item>Library </Menu.Item>
+        <Menu mode="inline">
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+          >
+            <Menu.Item style={{ padding: "1rem" }}>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.38rem",
+                }}
+              >
+                <div>
+                  <img src={DashboardIcon} alt="" />
+                </div>
+                <p>Dashboard</p>
+              </div>
+            </Menu.Item>
+            <Menu.Item style={{ padding: "1rem" }}>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.38rem",
+                }}
+              >
+                <div>
+                  <img src={SiderAssessmentIcon} alt="SiderAssessmentIcon" />
+                </div>
+                <p>Assessment</p>
+              </div>
+            </Menu.Item>
+            <Menu.Item style={{ padding: "1rem" }}>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.38rem",
+                }}
+              >
+                <div>
+                  <img src={LibraryIcon} alt="LibraryIcon" />
+                </div>
+                <p>Library</p>
+              </div>
+            </Menu.Item>
+          </div>
+          <Divider dashed></Divider>
+
+          <Menu.Item style={{ padding: "1rem", gap: "1rem" }}>
+            <div
+              style={{
+                gap: "0.7rem",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.38rem",
+                }}
+              >
+                <div>
+                  <img src={LibraryIcon} alt="LibraryIcon" />
+                </div>
+                <p>Round Status</p>
+                <Tag
+                  color="red"
+                  style={{
+                    borderRadius: "1.375rem",
+                    border: "1px solid var(--accent-new-red, #D63500)",
+                    background: "var(--accent-red-light, #FBEBEA)",
+                    color: "var(--accent-new-red, #D63500)",
+                    fontFamily: "Inter",
+                    fontSize: "0.625rem",
+                    fontStyle: "normal",
+                    fontWeight: "500",
+                    marginRight: 0,
+                    textAlign: "center",
+                  }}
+                >
+                  Admin
+                </Tag>
+              </div>
+            </div>
+          </Menu.Item>
         </Menu>
       </Drawer>
     </nav>
